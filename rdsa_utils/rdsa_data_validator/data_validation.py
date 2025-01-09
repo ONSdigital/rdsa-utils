e1c9a32da40bfa509931d5149af849b119c42970
James Westwood <james.westwood@ons.gov.uk>
2025-01-09 14:48:03 +0000

    WIP: Work so far on validator. TOML validator almost complete

--- rdsa_utils/rdsa_data_validator/data_validation.py
+++ rdsa_utils/rdsa_data_validator/data_validation.py
@@ -46,9 +46,7 @@ class GreatExpectationDataFrameValidator:
         """
         pass
 
-    def validate_dataframe_with_expectation_suite(self,
-                                                  dataframe,
-                                                  expectation_suite):
+    def validate_dataframe_with_expectation_suite(self, dataframe, expectation_suite):
         """Validate a DataFrame against a Great Expectations Expectation Suite.
 
         Args:
@@ -74,4 +72,3 @@ class GreatExpectationDataFrameValidator:
 
         """
         pass
-
